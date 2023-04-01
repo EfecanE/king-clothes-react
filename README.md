d01f00586e24f41edf9760eb37369887fd363ff3
EfecanE <mh.efecanefe@gmail.com>
2023-04-01 13:32:01 +0300

    some visual updates payment form

--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 
 <img width="1674" alt="King Clothes" src="https://user-images.githubusercontent.com/105597814/226259676-85ccd346-e169-4a2a-aa23-d7828ab96e21.png">
 
-
 ### What technologies did I use?
 
-* React
-* React Router
-* Redux
-* Firebase (Firebase Auth and Firestore)
-* Styled Components
-* Deploying with Netlify
+- React
+- React Router
+- Redux, Redux-Saga
+- Firebase (Firebase Auth and Firestore)
+- Styled Components
+- Stripe for payment
+- Netlify for deploying
 
 <a href="https://stalwart-tartufo-b75b83.netlify.app/" target="_blank">Live</a>
--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
+
+import { useSelector } from "react-redux";
+import { selectCartTotal } from "../../store/cart/cart.selector";
+import { selectCurrentUser } from "../../store/user/user.selector";
+
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 
-import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
-import { PaymentFormContainer, FormContainer } from "./payment-form.styles";
+import { BUTTON_TYPE_CLASSES } from "../button/button.component";
+import { PaymentFormContainer, FormContainer, PaymentButton } from "./payment-form.styles";
 
 const PaymentForm = () => {
   const stripe = useStripe();
   const elements = useElements();
+  const [isProcessingPayment, setIsProcessingPayment] = useState(false);
+  const amount = useSelector(selectCartTotal);
+  const currentUser = useSelector(selectCurrentUser);
 
   const paymentHandler = async (e) => {
     e.preventDefault();
@@ -14,12 +23,14 @@ const PaymentForm = () => {
       return;
     }
 
+    setIsProcessingPayment(true);
+
     const response = await fetch("/.netlify/functions/create-payment-intent", {
       method: "post",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ amount: 10000 }),
+      body: JSON.stringify({ amount: amount * 100 }),
     }).then((res) => res.json());
 
     const {
@@ -30,10 +41,12 @@ const PaymentForm = () => {
       payment_method: {
         card: elements.getElement(CardElement),
         billing_details: {
-          name: "Efecan Efe",
+          name: currentUser ? currentUser.displayName : "Guest",
         },
       },
     });
+
+    setIsProcessingPayment(false);
 
     if (paymentResult.error) {
       alert(paymentResult.error);
@@ -49,7 +62,7 @@ const PaymentForm = () => {
       <FormContainer>
         <h2>Credit Card Payment: </h2>
         <CardElement />
-        <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay Now</Button>
+        <PaymentButton isLoading={isProcessingPayment} buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay Now</PaymentButton>
       </FormContainer>
     </PaymentFormContainer>
   );
